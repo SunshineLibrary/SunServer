ed2623d8599e9bed060483ebb3085db1835ed609
Meng Lin <bluebird498@gmail.com>
2012-07-12 20:56:13 +0800

    remove drop_table migrations
    Change-Id: I32924153ac87a576b89377408649bdf778f9822a

--- db/migrate/20120712053656_drop_book.rb
+++
@@ -1,5 +0,0 @@
-class DropBook < ActiveRecord::Migration
-  def change
-    drop_table :books
-  end
-end
--- db/migrate/20120712073559_drop_author.rb
+++
@@ -1,5 +0,0 @@
-class DropAuthor < ActiveRecord::Migration
-  def change
-    drop_table :authors
-  end
-end
