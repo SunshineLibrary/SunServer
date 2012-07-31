9b51e2c5716b13078fb638db1ccd46134d2f44e9
mendlin <bluebird498@gmail.com>
2012-07-31 11:52:01 +0800

    fix a silly typo

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-  has_attached_file img_file, { 
+  has_attached_file :img_file, { 
         :path => ":rails_root/public/system/:attachment/:id/:style/:hash.:extension",
         :url => "/system/:attachment/:id/:style/:hash.:extension",
         :hash_secret => "longSecretString"
--- db/schema.rb
+++ db/schema.rb
@@ -174,7 +174,6 @@ ActiveRecord::Schema.define(:version => 20120730213736) do
     t.integer  "cover_s_file_size"
     t.datetime "cover_s_updated_at"
     t.integer  "difficulty"
-    t.integer  "hotness"
   end
 
   create_table "books_tags", :id => false, :force => true do |t|
@@ -206,7 +205,7 @@ ActiveRecord::Schema.define(:version => 20120730213736) do
     t.string   "editor"
     t.integer  "subject_id"
     t.integer  "grade"
-    t.integer  "semester"
+    t.string   "semester"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -288,7 +287,8 @@ ActiveRecord::Schema.define(:version => 20120730213736) do
   create_table "problems", :force => true do |t|
     t.string   "body"
     t.string   "tipe"
-    t.integer  "answer"
+    t.string   "answer"
+    t.integer  "order"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -371,6 +371,7 @@ ActiveRecord::Schema.define(:version => 20120730213736) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -382,13 +383,9 @@ ActiveRecord::Schema.define(:version => 20120730213736) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "classroom"
-    t.string   "school"
-    t.string   "user_type"
-    t.string   "birthday"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
   create_table "video_activities", :force => true do |t|
