9eca851636c90921d9be7aae0397c07fdcec6b9d
Meng Lin <bluebird498@gmail.com>
2012-08-08 16:58:18 +0800

    fix on production.rb, still test if

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,6 @@ SunServer::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
+  
+  Paperclip.options[:command_path] = "/usr/bin/"
 end
-
-Paperclip.options[:command_path] = "/usr/local/bin/"
