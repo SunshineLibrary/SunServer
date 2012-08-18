7c91c69e26a00c11882484c60b6c32220085cdb2
Meng Lin <bluebird498@gmail.com>
2012-08-19 01:01:35 +0800

    fix on tags delection and book edit page

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,6 +5,10 @@ class Tag < ActiveRecord::Base
   validates :name, :presence => true
   
   def Tag.type_list
-    Tag.all.map{|tag| tag.tag_type}.uniq    
+    Tag.select{|t| not t.destroyed?}.map{|tag| tag.tag_type}.uniq    
+  end
+  
+  def Tag.valid_tags wantted_tag_type    
+    Tag.where(tag_type: wantted_tag_type).select{|t| not t.destroyed?}    
   end
 end
