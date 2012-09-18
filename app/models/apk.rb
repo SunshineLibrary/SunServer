3da2062510097a30fe03aac546279355f4e4c29f
Bowen Sun <ukisami@gmail.com>
2012-09-18 11:14:35 +0800

    [bowen] fix apk latest

--- app/models/apk.rb
+++ app/models/apk.rb
@@ -1,7 +1,7 @@
 class Apk < ActiveRecord::Base
 
   scope :latest, lambda { select('apks.id, apks.name, apks.version').
-    joins('inner join (select name, max(version) as version from apks where created_at > 0 group by name) as a on apks.name = a.name and apks.version = a.version') }
+    joins("inner join (select name, max(version) as version from apks where created_at > '1970-01-01 00:00:00.000000' group by name) as a on apks.name = a.name and apks.version = a.version") }
 
   has_attached_file :file
 
