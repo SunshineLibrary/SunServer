0f77ffb507a5c51eef4afc5bb0c753c29278503f
mendlin <bluebird498@gmail.com>
2012-10-24 07:25:10 +0800

    Use 64bit k2pdfopt on Ali Cloud. PDF activity OK now.

--- gen_pdf_7.sh
+++ gen_pdf_7.sh
@@ -1,4 +1,4 @@
-EXEC=./k2pdfopt32
+EXEC=./k2pdfopt64
 FILE=$1
 
 $EXEC -ui- -w 480 -h 800 -odpi 133 -o %s_7 -x $FILE
--- gen_pdf_8.sh
+++ gen_pdf_8.sh
@@ -1,4 +1,4 @@
-EXEC=./k2pdfopt32
+EXEC=./k2pdfopt64
 FILE=$1
 
 $EXEC -ui- -w 768 -h 1024 -odpi 160 -o %s_8 -x $FILE
