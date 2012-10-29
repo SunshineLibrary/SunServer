c42a7286c73a6f01bd16ff83fa5a2b2781eb3472
linfeng <alexyangbg@gmail.com>
2012-10-29 15:55:36 +0800

    down for audio

--- db/migrate/20121029133459_add_audio_to_problems.rb
+++ db/migrate/20121029133459_add_audio_to_problems.rb
@@ -1,7 +1,14 @@
 class AddAudioToProblems < ActiveRecord::Migration
-  def change
+  def up
     change_table :problems do |t|
       t.has_attached_file :audio_file
     end
+  end
+  
+  def down
+    remove_column :problems, :audio_file_file_name
+    remove_column :problems, :audio_file_content_type
+    remove_column :problems, :audio_file_file_size
+    remove_column :problems, :audio_file_updated_at
   end
 end
