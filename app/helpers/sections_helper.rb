baf41fc570aa421523afdf8b59c467fb200748fb
Linfeng Yang <alexyangbg@gmail.com>
2012-12-13 01:01:25 +0800

    bugfix: cannot cancel in text activity edit.

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -16,25 +16,4 @@ module SectionsHelper
     r
   end
   
-  def get_all_lesson_option_list2
-    r = "\n"
-    Course.all.each do |c|
-      r << "<optgroup label='#{c.name}'>#{c.name}"
-      c.chapters.each do |ch|
-        r << "<option value=#{ch.id}>#{ch.name}</option>"
-      end
-      r << "</optgroup>"
-    end
-    r
-  end
-  
-  def get_all_lesson_option_list3
-    r = "<select name='course_id' class='input-xlarge' name='course_id'>\n\n"
-    Course.all.each do |c|
-      next if c.destroyed?
-      r << "<option value=#{c.id}>#{c.name}</option>\n\n"
-    end
-    r << "</select>"
-  end
-  
 end
